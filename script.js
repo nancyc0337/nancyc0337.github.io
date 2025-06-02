d5320e101416154bb90008ca9f6999d013b382cb
Nancy Chen <nancyc85@nycstudents.net>
2025-06-01 22:37:02 -0400

    add new files & modify old files & change css

--- script.js
+++
@@ -1,9 +0,0 @@
-let ownerName = "Nancy Chen"; // update the part BETWEEN the "quotes"
-let userName = "nancyc0337"; // same here
-
-document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
-});
-
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
